d61094d44d313430b330585ddd85c04d08e32eff
Junaid7200 <junaidjaffery1@gmail.com>
2025-12-20 17:43:52 +0500

    feat: code the degree service

---
+++ lib/src/services/degree_service.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+Future<List<Map<String, dynamic>>> getDegreesByDepartment(String depId) async {
+  final snapshot = await _firestore
+      .collection('degree')
+      .where('departmentId', isEqualTo: depId)
+      .get();
+  return snapshot.docs.map((doc) => doc.data()).toList();
+}
+
+Future<Map<String, dynamic>> getDegreeById(String degId) async {
+  final doc = await _firestore.collection('degree').doc(degId).get();
+  if (doc.exists) {
+    return doc.data()!;
+  } else {
+    throw Exception('Degree not found.');
+  }
+}
